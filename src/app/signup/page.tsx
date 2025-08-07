3aa2fdeff1764262fcb1e3e0d3ba7f6adae0c249
Shubham <shubham@topdoc.org>
2025-08-07 12:12:22 +0530

    Added transparent logo with size 32

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -59,7 +59,7 @@ export default function SignupPage() {
       <Card className="w-full max-w-md">
         <CardHeader className="text-center">
           <Link href="/" className="mb-4 flex items-center justify-center space-x-2">
-            <Image src="https://res.cloudinary.com/dfydjfauz/image/upload/v1754546915/favicon-32x32_gwtqvo.png" alt="First Impression Brand Logo" data-ai-hint="brand logo" width={28} height={28} />
+            <Image src="https://res.cloudinary.com/dfydjfauz/image/upload/v1754548771/apple-touch-icon_mwod6q.png" alt="First Impression Brand Logo" data-ai-hint="brand logo" width={32} height={32} />
             <span className="text-2xl font-bold font-headline">First Impression</span>
           </Link>
           <CardTitle className="text-2xl font-headline">Create an Account</CardTitle>
