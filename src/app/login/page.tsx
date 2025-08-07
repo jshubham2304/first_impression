9cb662355939dcf44cff904ee84c42aaedf804cb
Shubham Jain <jshubham2304@gmail.com>
2025-08-07 06:16:25 +0000

    https://res.cloudinary.com/dfydjfauz/image/upload/v1754546915/favicon-32

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -57,7 +57,7 @@ export default function LoginPage() {
       <Card className="w-full max-w-md">
         <CardHeader className="text-center">
             <Link href="/" className="mb-4 flex items-center justify-center space-x-2">
-           <Image src="https://res.cloudinary.com/dfydjfauz/image/upload/v1754546514/logo_nfxywf.png" alt="First Impression Brand Logo" data-ai-hint="brand logo" width={28} height={28} />
+           <Image src="https://res.cloudinary.com/dfydjfauz/image/upload/v1754546915/favicon-32x32_gwtqvo.png" alt="First Impression Brand Logo" data-ai-hint="brand logo" width={28} height={28} />
                 <span className="text-2xl font-bold font-headline">First Impression</span>
             </Link>
           <CardTitle className="text-2xl font-headline">Welcome Back</CardTitle>
